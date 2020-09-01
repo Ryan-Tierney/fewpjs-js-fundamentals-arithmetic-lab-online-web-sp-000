7a773730cb77d49f4baee5beaccc0ee19bc9d154
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-01 02:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // Write your code here
 var num1 = 31 
 var num2 = 2 
-var multiply = num1 * num2 
+var multiply = num1 * num2 ;
 
 var random = Math.random();
 
-var mod = 10 % 6 
+var mod = 10 % 6 ;
 
 var max = Math.max(20, 15, 12, 10);
